5651bcfa0010525edf850f8225c1a3fdccdb2e60
secondlocket <benjaminchan031@gmail.com>
2025-11-24 20:48:37 +0100

    buy geimplementeerd

--- finance/app.py
+++ finance/app.py
@@ -50,38 +50,6 @@ def index():
     return render_template("layout.html", message="WORK IN PROGRESS")
 
 
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-    """
-    TODO
-    make buy.html, maak finance.db af
-    """
-    symbol = request.form.get("symbol")
-    shares = request.form.get("shares")
-
-    # Checks
-    if not symbol:
-        return apology("must provide symbol", 400)
-
-    if symbol not in :
-        return apology("symbol not found", 400)
-    if shares < 0:
-        return apology("shares must be positive", 400)
-
-    # Submit the user's input via POST to /buy
-
-    info = lookup(symbol)
-
-    # Odds are you’ll want to SELECT how much cash
-    # the user currently has in users. (cash variable name)
-
-    """
-    if cash < price**********
-
-    return render_template("")
-    """
-
 @app.route("/history")
 @login_required
 def history():
@@ -104,18 +72,18 @@ def register():
         password2 = request.form.get("confirmation")
 
         if not username:
-            return apology("must provide username", 400)
+            return apology("must provide username")
         if not password1:
-            return apology("must provide password", 400)
+            return apology("must provide password")
         if not password2:
-            return apology("must confirm password", 400)
+            return apology("must confirm password")
 
         if password1 != password2:
-            return apology("passwords do not match", 400)
+            return apology("passwords do not match")
 
         user = db.execute("SELECT * FROM users WHERE username = ?", username)
         if len(user) != 0:
-            return apology("username already exists :(", 400)
+            return apology("username already exists :(")
 
         hashedpw = generate_password_hash(password1)
         new_id = db.execute("INSERT INTO users (username, hash) VALUES (?, ?)",
@@ -142,18 +110,18 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", code=403)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", code=403)
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return apology("invalid username and/or password", code=403)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -179,28 +147,72 @@ def logout():
 
 @app.route("/quote", methods=["GET", "POST"])
 @login_required
-# TODO
-# make quote.html and quoted.html
 def quote():
     """ TODO
     Get stock quote."""
     if request.method == "POST":
-        name = request.form.get("name")
-        price = request.form.get("price")
         symbol = request.form.get("symbol")
+        dict = lookup(symbol)
 
-        return render_template("quoted.html",
-            name = name,
-            price = price,
-            symbol = symbol
-        )
+        if dict is None:
+            return apology("Invalid symbol")
+        else:
+            name = dict['name']
+            price = dict['price']
+
+            return render_template("quoted.html",
+                name = name,
+                price = usd(price),
+                symbol = symbol
+            )
     else:
         return render_template("quote.html")
-    return apology("TODO")
+    return apology("POST/GET niet aangeroepen bij quote")
 
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    """Buy shares of stock"""
+    if request.method == "POST":
+        symbol = request.form.get("symbol")
+        shares = request.form.get("shares")
+        dict = lookup(symbol)
+
+        # Checks
+        if dict is None:
+            return apology("must provide valid symbol")
+        else:
+            price = dict['price']
+        if shares < 0:
+            return apology("shares must be positive")
+
+        total_cost = price * shares
+
+        # Odds are you’ll want to SELECT how much cash
+        # the user currently has in users. (cash variable name)
+        user_id = session["user_id"]
+        rows = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
+        cash = rows[0]["cash"]
+
+        if cash < total_cost:
+            return apology("not enough cash")
+
+        updated_cash = cash - total_cost
+
+        db.execute("UPDATE users SET cash = ? WHERE id = ?", updated_cash, user_id)
+        db.execute(
+            "INSERT INTO transactions (user_id, symbol, shares, price) VALUES (?, ?, ?, ?)",
+            user_id, symbol.upper(), shares, price
+        )
+        return redirect("/")
+    else:
+        return render_template("buy.html")
+
+    return apology("GET/POST niet aangeroepen bij buy")
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
 def sell():
     """Sell shares of stock"""
-    return apology("TODO")
+
+    return apology("")
